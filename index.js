db63b224de35745b611af6a45c8b8a2777ac1374
Ilancheyan <94479336+Ilancheyan@users.noreply.github.com>
2021-12-11 17:44:56 -0500

    Update index.js
    Added Crew model

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const {sequelize, DataTypes, Model} = require('./db')
+const {sequelize} = require('./db')
 //import models
 const { Movie } = require('./model/movie')
 const { Cast } = require('./model/cast')
+const { Crew } = require('./model/crew')
 
 //associate models
 //adds foreign key to cast table connecting a cast instance to a specific movie
 Cast.belongsTo(Movie)
+Crew.belongsTo(Movie)
 //gives us sequelize methods for a one to many relationship
 Movie.hasMany(Cast)
 
 //export models with added associations
-module.exports = {Movie, Cast, sequelize}
+module.exports = {Movie, Cast, Crew, sequelize}
