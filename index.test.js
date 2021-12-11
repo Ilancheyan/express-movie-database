169e1e68db79339eb0aa076da230759e60608669
Ilancheyan <94479336+Ilancheyan@users.noreply.github.com>
2021-12-11 17:43:50 -0500

    Update index.test.js
    Updated test

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {Movie, Cast, sequelize} = require('./index')
+const {Movie, Cast, Crew, sequelize} = require('./index')
 
 //test movie database CRUD
 describe('Movie Database', () => {
@@ -7,20 +7,49 @@ describe('Movie Database', () => {
         //reset database
         await sequelize.sync({force:true})
         //create array of movies
-        const arrayOfMovie = [
+        const arrayOfMovies = [
             {name: 'Cars', genre: 'Animation', rating: 'general'},
             {name: 'Kung Fu Panda', genre: 'Animation', rating: 'general'}
         ]
         //create array of casts
         const arrayOfCasts =[
-            {name: 'Owen Wilson', age: 45, gender: male},
-            {name: 'Paul Newman', age: 55, gender: male},
+            {name: 'Owen-Wilson', age: 40, gender: 'male'},
+            {name: 'Paul-Newman', age: 55, gender: 'male'},
 
         ]
+        //create array of crew
+        const arrayOfCrews =[
+            {name: 'John', position: 'script writer', company: 'Disney'},
+            {name: 'James', position: 'director', company: 'Pixar'},
+        
+        ]
+
         //add arrays to database
         await Movie.bulkCreate(arrayOfMovies)
         await Cast.bulkCreate(arrayOfCasts)
+        await Crew.bulkCreate(arrayOfCrews)
     })
+
+       //create instances of Movie Model for testing
+       test('movie has a name', async() => {
+        //read test instance from db
+        const testMovie = await Movie.findOne({where: {name: 'Cars'}});
+        expect(testMovie.name).toBe('Cars')
+    })
+
+        //create instances of Cast Model for testing
+           test('cast member has a name', async() => {
+            //read test instance from db
+            const testCast = await Cast.findOne({where: {name: 'Owen-Wilson'}});
+            expect(testCast.name).toBe('Owen-Wilson')
+        })
+        //create instances of Crew Model for testing
+           test('crew has a name', async() => {
+            //read test instance from db
+            const testCrew = await Crew.findOne({where: {name: 'John'}});
+            expect(testCrew.name).toBe('John')
+           })
+
     afterAll(async()=> {
         // await sequelize.sync({force:true})
         sequelize.close()
